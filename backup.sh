43d326f7d07550d0fe3f5bc33ac30a6452269c15
ANGEL NAVARRO <48390911+AngelNavarroR@users.noreply.github.com>
2020-09-10 09:09:15 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,21 +1,11 @@
-#!/usr/bin/env bash
-#!/usr/bin/expect -f 
-
-if ! `which expect 1>/dev/null`; then exit 1; yum install -y expect; fi 
-
-set timeout -1
-expect -c 'spawn ssh root@192.168.100.3 "mkdir -p /home/tesxx1/test"; 
-expect "assword:"; send "sisapp98\r";
-interact exit'
-
-#interact
-
-#mkdir -p /home/tesxx1/test
-
-
+#!/usr/bin/expect -f
+set timeout -1;
+spawn pg_dump -h localhost -p 5432 -U sisapp -F c ejemplo -f /home/ejemplo.backup;
+expect "Password:" ;
+send "sisapp98\n" ;
+interact
 exit 0;
 exit $?
-
 
 
 
